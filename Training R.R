8ce280b387adc6a8bb7d629bd35a3d919c41e7d8
Oluwashina Balogun <oluwashinabalogun2@gmail.com>
2020-08-14 11:32:38 +0100

    DS_R Programming_14-8-2020

--- Training R.R
+++ Training R.R
@@ -207,3 +207,85 @@ columnmean=function(y,removeNA=TRUE){
 }
 columnmean(airquality)
 columnmean(airquality,FALSE)  #BRINGING BACK THE NA
+
+##LOOP FUNCTIONS
+
+#lapply
+x=list(a=1:4, b=rnorm(10), c=rnorm(20,1), d=rnorm(100,5))
+lapply(x,mean)	##returning the means as a list
+
+x=1:4
+lapply(x,runif)
+
+x=1:4
+lapply(x,runif, min=0,max=10)
+
+#sapply
+x=list(a=1:4, b=rnorm(10), c=rnorm(20,1), d=rnorm(100,5))
+sapply(x,mean)	##returning the means as a vector
+
+#apply
+x=matrix(rnorm(200),20,10)
+apply(x,2,mean) ##preseving the columns
+apply(x,1,mean)	##preseving the rows
+
+x=matrix(rnorm(200),20,10)
+apply(x,1,quantile,probs=c(0.25,0.75))   ##returning the 25% & 75% of the rows
+
+x=array(rnorm(2*2*10),c(2,2,10))
+x
+apply(x,c(1,2),mean)		##mean of the 2X2 array across the 10 dimension
+
+OR
+
+rowMeans(x,dim=2)
+
+#mapply
+x=list(rep(1,4),rep(2,3),rep(3,2),rep(4,1))
+x
+mapply(rep,1:4,4:1)
+
+noise=function(n,mean,sd){
+  rnorm(n,mean,sd)			
+}
+noise(5,1,2)		
+noise(1:5,1:5,2)
+mapply(noise,1:5,1:5,2)			##VECTORIZING A FUNCTION(changing both n and mean but sd remain constant)
+
+#OR
+x=list(noise(1,1,2),noise(2,2,2),noise(3,3,2),noise(4,4,2),noise(5,5,2))
+x
+
+#tapply
+x=c(rnorm(10),runif(10),rnorm(10,1))
+f=gl(3,10)
+f
+tapply(x,f,mean)
+tapply(x,f,sum)
+tapply(x,f,range)
+
+#split
+x=c(rnorm(10),runif(10),rnorm(10,1))
+f=gl(3,10)
+split(x,f)
+lapply(split(x,f),mean)
+sapply(split(x,f),mean)
+
+x=split(airquality,airquality$Month)
+lapply(x,function(x)colMeans(x[,c("Ozone","Solar.R","Wind")]))
+
+x=split(airquality,airquality$Month)
+sapply(x,function(x)colMeans(x[,c("Ozone","Solar.R","Wind")]))
+
+x=split(airquality,airquality$Month)
+sapply(x,function(x)colMeans(x[,c("Ozone","Solar.R","Wind")],na.rm=TRUE))
+
+#splitting on more than one level
+x=rnorm(10)
+f1=gl(2,5)
+f2=gl(5,2)
+f1
+f2
+interaction(f1,f2)
+str(split(x,list(f1,f2)))
+str(split(x,list(f1,f2),drop=TRUE))
